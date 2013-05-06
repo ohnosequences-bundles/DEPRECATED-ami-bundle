2308ac5ce0108d2f83f1e17a372b3b16a58fc091
Alexey Alekhin <laughedelic@gmail.com>
2013-05-06 19:16:14 +0200

    Separated ami-id and ami-version fields

--- src/main/scala/AmiBundle.scala
+++ src/main/scala/AmiBundle.scala
@@ -4,14 +4,14 @@ import shapeless._
 import ohnosequences.statika.General._
 import scala.sys.process._
 
-abstract class CredentialsBundle(name: String, version: String) extends 
-  Bundle(name, version, dependencies = HNil: HNil) {
+abstract class CredentialsBundle(id: String, amiVersion: String, version: String) extends 
+  Bundle(id+"_"+amiVersion, version, dependencies = HNil: HNil) {
 
   override val install = {
     val ami = "ec2-metadata -a".!
-    if (ami == "ami-id: " + name)
+    if (ami == "ami-id: " + id)
     	success("Checked that Amazon Machine Image is " + this)
     else
-      failure("AMI should be "+ name +", whereas 'ec2-metadata -a' gives: "+ ami)
+      failure("AMI should be "+ id +", whereas 'ec2-metadata -a' gives: "+ ami)
   }
 }
