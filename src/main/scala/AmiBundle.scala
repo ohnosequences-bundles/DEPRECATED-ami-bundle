ad5aaf0a9cbb1209f40d64d3155f908630cda682
Alexey Alekhin <laughedelic@gmail.com>
2013-05-17 00:28:21 +0200

    Fixed ami version transformation

--- src/main/scala/AmiBundle.scala
+++ src/main/scala/AmiBundle.scala
@@ -5,12 +5,12 @@ import ohnosequences.statika.General._
 import scala.sys.process._
 
 abstract class AmiBundle(id: String, amiVersion: String, version: String) extends 
-  Bundle(id+"_"+amiVersion.replaceAll(".","_"), version, dependencies = HNil: HNil) {
+  Bundle(id+"_"+amiVersion.replaceAll("\\.","_"), version, dependencies = HNil: HNil) {
 
   override def install = {
     val ami = "wget -q -O - http://169.254.169.254/latest/meta-data/ami-id".!!.replaceAll("\n","")
     if (ami == id)
-      success("Checked that Amazon Machine Image is " + this)
+      success("Checked that Amazon Machine Image is " + id)
     else
       failure("AMI should be "+ id +", instead of "+ ami)
   }
