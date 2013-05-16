ff32379fbf953d1c3925dddd37f189d62cf31450
Alexey Alekhin <laughedelic@gmail.com>
2013-05-17 00:09:34 +0200

    Possible fix in getting ami id

--- src/main/scala/AmiBundle.scala
+++ src/main/scala/AmiBundle.scala
@@ -8,7 +8,7 @@ abstract class AmiBundle(id: String, amiVersion: String, version: String) extend
   Bundle(id+"_"+amiVersion.replaceAll(".","_"), version, dependencies = HNil: HNil) {
 
   override def install = {
-    val ami = "wget -q -O - http://169.254.169.254/latest/meta-data/ami-id".!!
+    val ami = "wget -q -O - http://169.254.169.254/latest/meta-data/ami-id".!!.replaceAll("\n","")
     if (ami == id)
       success("Checked that Amazon Machine Image is " + this)
     else
