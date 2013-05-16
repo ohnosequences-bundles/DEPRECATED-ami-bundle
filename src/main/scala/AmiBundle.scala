babe7bf56c48ab570f799cd8cc8d461fe206e46c
Alexey Alekhin <laughedelic@gmail.com>
2013-05-16 15:43:42 +0200

    Added user-script generation method

--- src/main/scala/AmiBundle.scala
+++ src/main/scala/AmiBundle.scala
@@ -7,11 +7,13 @@ import scala.sys.process._
 abstract class AmiBundle(id: String, amiVersion: String, version: String) extends 
   Bundle(id+"_"+amiVersion.replaceAll(".","_"), version, dependencies = HNil: HNil) {
 
-  override val install = {
+  override def install = {
     val ami = "wget -q -O - http://169.254.169.254/latest/meta-data/ami-id".!!
     if (ami == id)
       success("Checked that Amazon Machine Image is " + this)
     else
       failure("AMI should be "+ id +", instead of "+ ami)
   }
+
+  def userScript(b: BundleAux)(implicit s: Selector[b.Deps, this.type]): String
 }
