653c5ca914b8f25615b4064a9b3e443fe00d67e8
Alexey Alekhin <laughedelic@gmail.com>
2013-05-26 07:16:02 +0200

    Updated for statika-0.10.0 and sbt-statika-0.1.1

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,8 @@
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.6")
+resolvers ++= Seq (
+  Resolver.url("Era7 Ivy Snapshots", url("http://snapshots.era7.com.s3.amazonaws.com"))(
+    Patterns("[organisation]/[module]/[revision]/[type]s/[artifact].[ext]"))
+, Resolver.url("Era7 Ivy Releases", url("http://releases.era7.com.s3.amazonaws.com"))(
+    Patterns("[organisation]/[module]/[revision]/[type]s/[artifact].[ext]"))
+)
+
+addSbtPlugin("ohnosequences" % "sbt-statika" % "0.1.1")
--- src/main/scala/AmiBundle.scala
+++ src/main/scala/AmiBundle.scala
@@ -2,27 +2,28 @@ package ohnosequences.statika
 
 import shapeless._
 import ohnosequences.statika.General._
+import ohnosequences.statika.TypeLookupHList._
 import scala.sys.process._
 
-abstract class AmiBundle(
-    val id: String
-  , val amiVersion: String
-  , version: String
-  ) extends 
-  Bundle( name = id.split("""\W""").map(_.capitalize).mkString +
-                 "_" + amiVersion.replaceAll("\\.","_")
-        , version
-        , artifact = id+"."+amiVersion
-        , dependencies = HNil: HNil) {
+object Ami {
 
-  override def install = {
-    val ami = "wget -q -O - http://169.254.169.254/latest/meta-data/ami-id".!!.replaceAll("\n","")
-    if (ami == id)
-      success("Checked that Amazon Machine Image is " + id)
-    else
-      failure("AMI should be "+ id +", instead of "+ ami)
+  abstract class AmiBundle(val id: String, val amiVersion: String)
+    (implicit meta: BundleMetaData) extends BundleWithMetaData(data = meta) {
+
+    override def install = {
+      val ami = "wget -q -O - http://169.254.169.254/latest/meta-data/ami-id".!!.replaceAll("\n","")
+      if (ami == id)
+        success("Checked that Amazon Machine Image is " + id)
+      else
+        failure("AMI should be "+ id +", instead of "+ ami)
+    }
+
+    def userScript[B <: BundleAux](b: B)
+        (implicit s: Selector[b.Deps, this.type]): String
   }
 
-  def userScript[B <: BundleAux](b: B)
-      (implicit s: Selector[b.Deps, this.type]): String
+  implicit class GetBundlesAmi[B <: BundleAux](b: B) {
+    def ami(implicit a: Contains[b.Deps, AmiBundle]): a.Out = a(b.dependencies) 
+  }
+ 
 }
